7af3921d72a823dbec8b6e4604a4800a05e29173
Saberqadimi <qadimi43@gmail.com>
2022-07-04 17:20:45 +0430

    install vueCli&vueCreateProject & *pass DataFrom child And passData From Parent* Props and Emit is learned

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- پاس دادن دیتا بصورت آرایه -->
+    <!-- <Home :user-name="name"/> -->
+    <!-- پاس دادن دیتا بصورت آبجکت -->
+    <!-- <Home :user-data="userInfo" /> -->
+
+    <!-- دریافت دیتا از کلاس فرزند -->
+   <Home :user-data="userInfo"  @emitChild="showData($event)" />
+  
+   <h1>App.vuejs</h1>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from "./components/Home.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+export default { 
+  components: { Home } , 
+  data() {
+    return {
+      // متغیرآرایه
+      // name:'data passed from App.vue',
+      userInfo: {
+        name:'saber',
+        lastname: 'qadimi',
+        age: 27
+      }
+    }
+  }, 
+  methods: {
+    showData(name){
+      this.userInfo.name = name
+    }
+  },
+  
+  
+  
+  
+  
+  
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <!-- نمایش دادن دیتا بصورت آرایه -->
+  <!-- <h1>{{userName}}</h1> -->
+
+  <ul>
+    <li>{{ userData.name }}</li>
+    <li>{{ userData.lastname }}</li>
+    <li>{{ userData.age }}</li>
+  </ul>
+
+  <button @click="sendData">send Data for App.vue</button>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  //  گرفتن دیتا بصورت آرایه
+  //    props:['userName']
+  //  گرفتن دیتا بصورت آبجکت
+  props: {
+    userData: Object,
+  },
+
+   data() {
+       return {
+           name : 'JustLearn'
+       }
+
+   },
+
+    methods: {
+        sendData(){
+            this.$emit('emitChild' , this.name)
+        }
+    },
+
+
+
+
+
+}
+</script>
+
+<style scoped>
+ul {
+  background-color: beige;
+  padding: 6px;
+  text-align: center;
+  list-style: none;
+}
+li {
+  color: blue;
+  padding-top: 10px;
+  font-size: 22px;
+}
+</style>
